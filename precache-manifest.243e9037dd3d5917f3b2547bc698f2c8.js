a1bec44509ff0d452fedc7bdad4c23818001e892
Jeff Lung <nosy_parker@msn.com>
2019-05-01 16:34:21 +0800

    Updates

---
+++ precache-manifest.243e9037dd3d5917f3b2547bc698f2c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "44340542c48e10c20cd6",
+    "url": "/portfolio-app/static/js/runtime~main.cb97d77a.js"
+  },
+  {
+    "revision": "60f1c2b2777f2f295644",
+    "url": "/portfolio-app/static/js/main.725114f8.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "60f1c2b2777f2f295644",
+    "url": "/portfolio-app/static/css/main.835ca931.chunk.css"
+  },
+  {
+    "revision": "9e4da65b5ef44b61e2bcb0a24a00fa82",
+    "url": "/portfolio-app/index.html"
+  }
+];
